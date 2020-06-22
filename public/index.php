93f36158918b207e5bfba1a20ad586e82212d935
AndreiChornii <andrei.chornii@gmail.com>
2020-06-22 17:08:46 +0300

    singl_entry_point

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+echo 'hello.world';
+include './handlers/dump.php';
+include './handlers/mysqli.php';
+include './handlers/validate.php';
+
+$route = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+include './handlers/routesMethodGet.php';
+
+include './handlers/routesMethodPost.php';
